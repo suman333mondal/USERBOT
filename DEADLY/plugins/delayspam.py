b78d1ed302c53ab1a4805afafff8f9dd5422f2aa
SUMAM MONDAL <118373042+suman333mondal@users.noreply.github.com>
2023-01-31 16:11:53 +0530

    Update delayspam.py

--- DEADLY/plugins/delayspam.py
+++ DEADLY/plugins/delayspam.py
@@ -12,7 +12,7 @@ from resources.data import *
 
 @Client.on_message(filters.user(SUDOERS) & filters.command(["delayspam"], [",", ".", "!", "/", "+", "?"]))
 async def delayspam(app: Client, m:Message):
-    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗹𝗮𝘆𝗦𝗽𝗮𝗺\n\nCommand:\n\n.delayspam [sleep time] [count] [message to spam] \n\n.delayspam [sleep time] [count] [reply to a message]\n\nCount and Sleeptime must be a integer."     
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.delayspam [sleep time] [count] [message to spam] \n\n.delayspam [sleep time] [count] [reply to a message]\n\nCount and Sleeptime must be a integer."     
     Deadly = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
     Deadlysexy = Deadly[1:]
     if len(Deadlysexy) == 2:
